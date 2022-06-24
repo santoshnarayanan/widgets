9d65296afcac3945c4e5444897eaf472c16f91a4
santosh33 <santosh33@gmail.com>
2022-06-24 17:09:20 +0530

    Provide button with link to respective page

--- src/components/Search.js
+++ src/components/Search.js
@@ -24,6 +24,24 @@ const Search = () => {
         
     },[term]);
 
+    const renderedResults = results.map((result)=>{
+        return(
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>
+                    Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html:result.snippet}}></span>
+                </div>
+            </div>
+        );
+    })
+
     return (
         <div>
             <div className="ui form">
@@ -33,6 +51,7 @@ const Search = () => {
                     onChange={e=> setTerm(e.target.value)} className="input"/>
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 };

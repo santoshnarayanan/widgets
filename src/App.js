9ce47414ee198f2d65476ad8a8d14277ef463623
santosh33 <santosh33@gmail.com>
2022-07-10 17:57:12 +0530

    Basic component routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React, { useState } from "react";
-//import Accordion from "./components/Accordion";
-//import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/translate";
 
-// const items =[
-//     {
-//         title: 'What is React?',
-//         content: 'React is javascript front-end framework'
-//     },
-//     {
-//         title: 'Why use React?',
-//         content: 'React is favourite js library'
-//     },
-//     {
-//         title: 'How do you use React?',
-//         content: 'React is used by creating components'
-//     }
+const items =[
+    {
+        title: 'What is React?',
+        content: 'React is javascript front-end framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is favourite js library'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'React is used by creating components'
+    }
 
-// ];
+];
 
 const options = [
   {
@@ -35,11 +35,42 @@ const options = [
   },
 ];
 
+//! shows menu item
+const showAccordion = () =>{
+  if(window.location.pathname === '/'){
+    return <Accordion items={items} />;
+  }
+}
+
+//! shows menu item
+const showList = () =>{
+  if(window.location.pathname === '/list'){
+    return <Search />;
+  }
+}
+
+//! shows menu item
+const showDropDown = () =>{
+  if(window.location.pathname === '/dropdown'){
+    return <Dropdown />;
+  }
+}
+
+//! shows menu item
+const showTranslate = () =>{
+  if(window.location.pathname === '/translate'){
+    return <Translate />;
+  }
+}
+
 const App = () => {
 
   return (
     <div>
-        <Translate />
+        {showAccordion()};
+        {showList()}
+        {showDropDown()}
+        {showTranslate()}
     </div>
   );
 };

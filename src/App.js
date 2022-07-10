b1f09e846cf7f3abf12a1e9f2e7126749d77b43c
santosh33 <santosh33@gmail.com>
2022-07-10 18:12:44 +0530

    Created Routing Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React,{useState} from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/translate";
+import Route from "./components/Route";
 
 const items =[
     {
@@ -35,42 +36,27 @@ const options = [
   },
 ];
 
-//! shows menu item
-const showAccordion = () =>{
-  if(window.location.pathname === '/'){
-    return <Accordion items={items} />;
-  }
-}
-
-//! shows menu item
-const showList = () =>{
-  if(window.location.pathname === '/list'){
-    return <Search />;
-  }
-}
-
-//! shows menu item
-const showDropDown = () =>{
-  if(window.location.pathname === '/dropdown'){
-    return <Dropdown />;
-  }
-}
-
-//! shows menu item
-const showTranslate = () =>{
-  if(window.location.pathname === '/translate'){
-    return <Translate />;
-  }
-}
-
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
 
+  //! Here items property is passed as children to Route
   return (
     <div>
-        {showAccordion()};
-        {showList()}
-        {showDropDown()}
-        {showTranslate()}
+        <Route path="/">
+          <Accordion  items={items}/> 
+        </Route>
+        <Route path="/list">
+          <Search /> 
+        </Route>
+        <Route path="/dropdown">
+          <Dropdown  label = "Select a color" 
+              options={options}
+              selected={selected} 
+              onSelectedChange={setSelected}/> 
+        </Route>
+        <Route path="/translate">
+          <Translate /> 
+        </Route>
     </div>
   );
 };
